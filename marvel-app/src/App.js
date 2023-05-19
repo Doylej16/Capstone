d10833a5e10e01f48234a83e80a4fbe423df5e39
jmeza777 <super0268@gmail.com>
2023-05-18 18:00:27 -0700

    added card with thumbnail, name, description, copyright

--- marvel-app/src/App.js
+++ marvel-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
+import FetchData from './Components/FetchData';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="">
      <Signup/>
+     <FetchData/>
     </div>
   );
 }
