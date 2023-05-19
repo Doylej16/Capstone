2add3250b5835c23c71de67339cf6319ef5d247c
jmeza777 <super0268@gmail.com>
2023-05-18 18:25:34 -0700

    added card with info

--- marvel-app/src/App.js
+++ marvel-app/src/App.js
@@ -4,13 +4,15 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './Components/Login';
 import Navbar from './Components/navbar';
 import Signup from './Components/Signup';
-import Login from './Components/Login';
+import FetchData from './Components/FetchData';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="">
      <Signup/>
+     <FetchData/>
     </div>
   );
 }
