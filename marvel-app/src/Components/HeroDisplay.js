dc0e5f77e4740f835a43c8c283e437f4d050b430
Alex Quezada Magdaleno <alexquezadamagdaleno@gmail.com>
2023-06-01 20:15:52 -0400

    favs

--- marvel-app/src/Components/HeroDisplay.js
+++ marvel-app/src/Components/HeroDisplay.js
@@ -1,66 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import axios from 'axios';
 
-const HeroDisplay = ({ heroSearch, currentUser }) => {
+const HeroDisplay = ({ heroSearch, addToFavorites }) => {
   const thumbnail = heroSearch[0].thumbnail.path + '.' + heroSearch[0].thumbnail.extension;
-  const attributionText = 'Data provided by Marvel';
+  const attributionText = 'Data provided by Marvel.';
 
-  const [favorites, setFavorites] = useState([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    fetchFavorites();
-  }, []);
-
-  const fetchFavorites = async () => {
-    try {
-      const response = await axios.get('http://localhost:3100/api/favorites');
-      setFavorites(response.data);
-    } catch (error) {
-      setError('Failed to fetch favorites. Please try again later.');
-    }
-  };
-
-  const addToFavorites = async (characterId) => {
-    try {
-      if (!currentUser || !currentUser.email) {
-        setError('Current user not found. Please log in again.');
-        return;
-      }
-
-      const response = await axios.post('http://localhost:3100/api/addFavorite', {
-        marvelCharacterId: characterId,
-      });
-
-      if (response.status === 200 && response.data.success) {
-        fetchFavorites();
-      } else {
-        setError('Failed to add character to favorites. Please try again later.');
-      }
-    } catch (error) {
-      setError('An error occurred while adding the character to favorites. Please try again later.');
-    }
+  const handleAddToFavorites = () => {
+    addToFavorites(heroSearch[0].id); // Pass the Marvel character ID to the addToFavorites function
   };
 
   return (
     <>
-      {error && <p className="error-message">{error}</p>}
-      <div className="flex justify-center">
+      <div className='flex justify-center'>
         <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src={thumbnail} />
+          <Card.Img
+            className='transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+            variant="top"
+            src={thumbnail}
+          />
           <Card.Body>
-            <Card.Title className="text-lg">Name: {heroSearch[0].name}</Card.Title>
+            <Card.Title className='text-lg'>Name: {heroSearch[0].name}</Card.Title>
             <Card.Text>
-              <p className="font-mono text-lg">{heroSearch[0].description}</p>
-              <p className="font-mono text-lg">{attributionText}</p>
+              <p className='font-mono text-lg'>{heroSearch[0].description}</p>
+              <p className='font-mono text-lg'>{attributionText}</p>
             </Card.Text>
-            <Button
-              className="text-lg bg-green-600 rounded-md"
-              variant="danger"
-              onClick={() => addToFavorites(heroSearch[0].id)}
-            >
-              Add to Favorites
+            <Button className='text-lg bg-green-600 rounded-md' variant="danger" onClick={handleAddToFavorites}>
+              Add to Favorite
             </Button>
           </Card.Body>
         </Card>
